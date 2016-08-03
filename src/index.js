b7d1ea91e5647589fc16bd271443d73ad4203d38
Kirill Shumilov <brusher.designer@gmail.com>
2016-08-03 11:53:19 +0300

    Make component import font-awesome.scss instead of css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @copyright 2015, Andrey Popp <8mayday@gmail.com>
  */
 
-import 'font-awesome/css/font-awesome.css';
+import 'font-awesome/scss/font-awesome.scss';
 
 import Icon from './Icon';
 import IconStack from './IconStack';
